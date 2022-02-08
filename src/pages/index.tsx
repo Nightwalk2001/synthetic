5e2520b4427d832cd7ab04b708f57c5ac73be05a
user <3440771474@qq.com>
2022-02-08 08:57:24 +0800

    终版

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,16 @@ const Home = () => {
   const toProject = () => router.push("/background")
   const toTeam = () => router.push("/team")
 
-  return <div className={"fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[900px] mx-auto"}>
+  return <div className={"fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[950px] mx-auto"}>
     <div className={"flex flex-col items-center w-full mx-auto py-4 text-center"}>
       <div className={"mb-9 text-7xl text-gray-900 font-black tracking-wide"}>
         多细胞模块共催化合成高价值产品的应用。
       </div>
-      <div className={"px-[150px] font-ff text-xl text-gray-600"}>
-        探究使用多菌共催化体系合成酪醇及其衍生物。
+      <div className={"w-[620px] font-ff text-2xl text-gray-600 tracking-wider"}>
+        探究使用人工双菌体系模块共催化合成酪醇，并对体系进行优化，在此基础上使用三菌体系，模块共催化合成酪醇衍生物红景天苷和羟基酪醇。
       </div>
 
-      <div className={"flex my-10"}>
+      <div className={"flex my-8"}>
         <button
           onClick={toProject}
           className={"mr-12 px-3.5 py-2.5 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-400"}>
